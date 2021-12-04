d03a255f7a944fb579cce194a41643b79cd711f3
timothee-picard <timothee@atnos.com>
2021-12-04 18:03:45 +0100

    feat: add algo foreach

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 import recipes from "./recipes.js"
 
-const input = document.getElementById('input');
-const result = document.getElementById('result');
+const $inputIngredients = document.getElementById('ingredients')
+const $inputUstensiles = document.getElementById('ustensiles')
+const $inputAppareil = document.getElementById('appareil')
+const $inputRecherche = document.getElementById('recherche')
 
+// Barre principale
+$inputRecherche.addEventListener('input', (e)=> {
+  if(e.target.value.length >= 3) {
+    var recette = []
+    recipes.forEach((rec)=> {
+      // on ajoute les recettes contenant le mot recherché dans le nom ou la desc
+      if(rec.name.toUpperCase().includes(e.target.value.toUpperCase()) ||
+      rec.description.toUpperCase().includes(e.target.value.toUpperCase())) {
+        recette.push(rec)
+      } 
+      else {
+        //on cherche dans les ingrédients
+        rec.ingredients.forEach((ing)=> {
+          (ing.ingredient.toUpperCase().includes(e.target.value.toUpperCase()))? recette.push(rec) : null;
+        })
+      }
+    })
+    console.log(recette)
+  }
+})
 
 // Récupérer la liste des ingrédients
 var ingredientsList = []
@@ -13,18 +35,84 @@ recipes.forEach((recipe)=> {
 })
 ingredientsList = [...new Set(ingredientsList)]
 
+// Récupérer la liste des ustensiles
+var ustensilesList = []
+recipes.forEach((recipe)=> {
+    recipe.ustensils.forEach((ustensilsInfo)=> {
+      ustensilesList.push(ustensilsInfo.toUpperCase())
+    })
+})
+ustensilesList = [...new Set(ustensilesList)]
+
+// Récupérer la liste des appareils
+var appareilsList = []
+recipes.forEach((recipe)=> {
+    appareilsList.push(recipe.appliance.toUpperCase())
+})
+appareilsList = [...new Set(appareilsList)]
 
 
-input.addEventListener('input', (e)=> {
-    if(e.target.value.length >= 3) {
-        result.innerHTML = e.target.value;
-        
-        var test = []
-        ingredientsList.forEach((ing)=> {
-            console.log(ing.includes(e.target.value))
-            
+$inputIngredients.addEventListener('input', (e)=> {
+  if(e.target.value.length >= 3) {
+      //récupérer la liste des ingrédients en fonction de la recherche
+      var ingredients = []
+      ingredientsList.forEach((ing)=> {
+        (ing.includes(e.target.value.toUpperCase())) ?  ingredients.push(ing) : null;     
+      })
+      console.log(ingredients)
+
+      //récupérer la liste des recettes concernées
+      var recette = []
+      recipes.forEach((rec)=> {
+        rec.ingredients.forEach((ing)=> {
+          ingredients.forEach((e)=> {
+            (ing.ingredient.toUpperCase() == e)? recette.push(rec) : null;
+          })
         })
+      })
+      console.log(recette)
+  }
+})
 
-        console.log([...new Set(test)])
-    }
-});
+$inputUstensiles.addEventListener('input', (e)=> {
+  if(e.target.value.length >= 3) {
+      //récupérer la liste des ustensiles en fonction de la recherche
+      var ustensiles = []
+      ustensilesList.forEach((ust)=> {
+        (ust.includes(e.target.value.toUpperCase())) ?  ustensiles.push(ust) : null;     
+      })
+      console.log(ustensiles)
+
+      // récupérer la liste des recettes concernées
+      var recette = []
+      recipes.forEach((rec)=> {
+        rec.ustensils.forEach((ust)=> {
+          ustensiles.forEach((e)=> {
+            (ust.toUpperCase() == e)? recette.push(rec) : null;
+          })
+        })
+      })
+      console.log(recette)
+  }
+})
+
+
+$inputAppareil.addEventListener('input', (e)=> {
+  if(e.target.value.length >= 3) {
+      //récupérer la liste de l'appareil en fonction de la recherche
+      var appareil = []
+      appareilsList.forEach((appa)=> {
+        (appa.includes(e.target.value.toUpperCase())) ?  appareil.push(appa) : null;     
+      })
+      console.log(appareil)
+
+      //récupérer la liste des recettes concernées
+      var recette = []
+      recipes.forEach((rec)=> {
+        appareil.forEach((appa)=> {
+          (appa == rec.appliance.toUpperCase())? recette.push(rec) : null;
+        })
+      })
+      console.log(recette)
+  }
+})
